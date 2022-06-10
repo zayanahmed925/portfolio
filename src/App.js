81503761d3b909d0e60556695457936f4ca66e5f
zayanahmed925 <rjraju925@gmail.com>
2022-06-10 23:44:21 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Page/Home/Home';
+import Navbar from './Page/Home/Navbar';
 import ProjectDetail from './Page/ProjectDetail/ProjectDetail';
 
 function App() {
   return (
     <div >
-
+      <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/project/:projectId' element={<ProjectDetail></ProjectDetail>}></Route>
--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
-import Navbar from './Navbar';
 import Project from './Project/Project';
 
 const Home = () => {
     return (
         <div>
-            <Navbar></Navbar>
             <Banner></Banner>
             <Project></Project>
             <Contact></Contact>
